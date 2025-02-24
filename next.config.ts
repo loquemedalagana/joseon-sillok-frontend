19a38de6bb064a8067e18a530a17ec3df7bb887b
loquemedalagana <nadamasquetu012@gmail.com>
2025-02-24 21:09:08 +0900

    - fix deploy error

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
+
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 const nextConfig: NextConfig = {
-  output: "export", // Static Export 활성화 (SSG)
-  assetPrefix: process.env.GITHUB_PAGES ? '/joseon-sillok-frontend/' : '', // GitHub Pages 경로 설정
+  output: 'export', // Static Export 설정
+  assetPrefix: isGitHubPages ? '/joseon-sillok-frontend/' : '',
+  basePath: isGitHubPages ? '/joseon-sillok-frontend' : '', // GitHub Pages 경로 설정
   images: {
-    unoptimized: true, // 이미지 최적화 비활성화 (정적 사이트)
+    unoptimized: true, // 이미지 최적화 비활성화
   },
 };
 
